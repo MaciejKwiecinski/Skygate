7187284526af990998e7b3e6c01d65dec3217ee8
MaciejKwiecinski <mackwiecinski@gmail.com>
2019-04-01 17:18:07 +0200

    views+API

--- manage_exams/admin.py
+++ manage_exams/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Exam,Exercise
 
 # Register your models here.
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('subject','points','grades','student')
+admin.site.register(Exam, ExamAdmin)
+
