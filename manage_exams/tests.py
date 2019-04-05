388f86bd83156e288d67439bf23a980092fe6349
MaciejKwiecinski <mackwiecinski@gmail.com>
2019-04-05 16:08:57 +0200

    tests

--- manage_exams/tests.py
+++ manage_exams/tests.py
@@ -1,26 +1,40 @@
 from django.urls import reverse
 from django.test import TestCase
+from .models import User,Exam,Exercise
 
-class ExamTest(TestCase):
+class Connection(TestCase):
+
     def exam(self):
         response = self.client.get(reverse('exam'))
         self.assertEqual(response.status_code, 200),
 
-class ExerciseTest(TestCase):
     def exercise(self):
         response = self.client.get(reverse('exercise'))
         self.assertEqual(response.status_code, 200),
 
-class ExerciseListTest(TestCase):
     def exam_list(self):
         response = self.client.get(reverse('exams_list'))
         self.assertEqual(response.status_code, 200),
 
-class ExamListTest(TestCase):
     def exercise_list(self):
         response = self.client.get(reverse('exercise_list'))
         self.assertEqual(response.status_code, 200),
 
 
-# Unfortunately, my tests are quite poor, because the only thing I check,
-# is the returned value, and I get nothing else except status_code 200
+class ModelTest(TestCase):
+
+    def test_user(self):
+        test_user = User.objects.get(id = 1)
+        self.assertIsInstance(test_user, User)
+        self.assertEqual(test_user.username, 'Maciej')
+        self.assertEqual(test_user.is_staff, True)
+
+    def test_exam(self):
+        test_exam = Exam.objects.get(id=3)
+        self.assertIsInstance(test_exam, Exam)
+        self.assertEqual(test_exam.subject, 2)
+
+    def test_exercises(self):
+        test_exercises = Exercise.objects.get(id=1)
+        self.assertIsInstance(test_exercises, Exercise)
+        self.assertEqual(test_exercises.points, 1)
